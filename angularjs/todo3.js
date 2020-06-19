3e44baeed93e056b98f2d7020c319755ec73582c
synetcom1 <tech.headinstructor@ciccc.ca>
2020-06-19 11:37:58 -0700

    corrected

--- angularjs/todo3.js
+++ angularjs/todo3.js
@@ -1,21 +1,30 @@
-angular.module('todoApp', []).controller('todoController', ['$scope', function ($scope) {
-    let load = function () {
-        let raw_data = localStorage.getItem('todo');
-        if (raw_data === null) {
-            $scope.tasks = [];
-        } else {
-            $scope.tasks = JSON.parse(raw_data);
+angular.module('todoApp', [])
+    .controller('todoController', ['$scope', function ($scope) {
+        let load = function () {
+            let raw_data = localStorage.getItem('todo');
+            if (raw_data === null) {
+                $scope.tasks = [];
+            } else {
+                // javascript object
+                $scope.tasks = JSON.parse(raw_data);
+            }
+        };
+        let save = function () {
+            // to string 
+            localStorage.setItem('todo', JSON.stringify($scope.tasks));
         }
-    };
-    let save = function () {
-        localStorage.setItem('todo', JSON.stringfy($scope.taks));
-    }
-    $scope.add = function () {
-        $scope.tasks.push($scope.title);
-        save();
-    }
-    $scope.delete() = function () {
-        $scope.tasks.splice(this.$index, 1);
-        save();
-    }
-}]);
+        $scope.add = function () {
+            $scope.tasks.push($scope.title);
+            save();
+        }
+        $scope.delete = function () {
+            $scope.tasks.splice(this.$index, 1);
+            save();
+        }
+        load();
+    }]);
+
+
+
+
+
